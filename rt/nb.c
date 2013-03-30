755dce27fac264515df220545adde3ea30d08b32
Tastyep <luc.sinet@epitech.eu>
2013-03-31 00:50:43 +0100

    enhanced my_atof

--- rt/nb.c
+++ rt/nb.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sat Oct 20 11:27:34 2012 luc sinet
-** Last update Sun Mar 17 03:08:36 2013 luc sinet
+** Last update Sun Mar 31 00:10:38 2013 luc sinet
 */
 
 #include <stdlib.h>
@@ -57,53 +57,6 @@ int    	my_getnbr(char *s1)
       nb = nb * 10 + (s1[i] - 48);
       i++;
     }
-  return (nb * sign);
-}
-
-double		get_deci(char *s1)
-{
-  double	res;
-  double	deci;
-  int		pos;
-
-  deci = 1;
-  pos = 0;
-  res = 0;
-  while (s1[pos] >= '0' && s1[pos] <= '9')
-    {
-      if (res * 10 / 10 != res)
-	return (0);
-      res = res * 10 + (s1[pos] - 48);
-      ++pos;
-      deci *= 10;
-    }
-  return (res / deci);
-}
-
-double		my_fgetnbr(char *s1)
-{
-  int		i;
-  double       	nb;
-  double       	sign;
-
-  nb = 0;
-  i = 0;
-  sign = 1;
-  while (s1[i] == ' ' || s1[i] == '-' || s1[i] == '+')
-    {
-      if (s1[i] == '-')
-	sign = -sign;
-      i++;
-    }
-  while (s1[i] >= '0' && s1[i] <= '9')
-    {
-      if (((nb * 10) / 10) != nb)
-	  return (0);
-      nb = nb * 10 + (s1[i] - 48);
-      i++;
-    }
-  if (s1[i] == ',' || s1[i] == '.')
-    nb += get_deci(&s1[i + 1]);
   return (nb * sign);
 }
 
