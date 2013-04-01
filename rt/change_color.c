37f62ed843722e365de10913ca1559b6e3bdd7e4
Tastyep <luc.sinet@epitech.eu>
2013-04-01 17:55:51 +0200

    enhanced color application and started multi spot

--- rt/change_color.c
+++ rt/change_color.c
@@ -5,23 +5,24 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Thu Mar 21 16:50:09 2013 luc sinet
-** Last update Mon Apr  1 17:20:19 2013 luc sinet
+** Last update Mon Apr  1 17:53:59 2013 luc sinet
 */
 
 #include "main.h"
 #include "light.h"
 #include "change_color.h"
 
-void		apply_brightness(unsigned char *col_o, unsigned char *col_l)
+void		apply_brightness(unsigned char *col_o, unsigned char *col_l,
+				 double lg_i)
 {
   double	a;
   double	b;
 
-  a = (4.0 / 10.0);
+  a = ((6.0 / 10.0) * lg_i);
   b = 1.0 - a;
-  col_o[0] = a * (double)col_o[0] + b * (double)col_l[0];
-  col_o[1] = a * (double)col_o[1] + b * (double)col_l[1];
-  col_o[2] = a * (double)col_o[2] + b * (double)col_l[2];
+  col_o[0] = b * (double)col_o[0] + a * (double)col_l[0];
+  col_o[1] = b * (double)col_o[1] + a * (double)col_l[1];
+  col_o[2] = b * (double)col_o[2] + a * (double)col_l[2];
 }
 
 
@@ -31,7 +32,7 @@ unsigned int	apply_light(unsigned int color, double cosa,
   unsigned char	c[3];
 
   decomp_color(color, c);
-  apply_brightness(c, lpt->lcolor);
+  apply_brightness(c, lpt->lcolor, cosa);
   c[0] = (double)c[0] * cosa * ept->bright;
   c[1] = (double)c[1] * cosa * ept->bright;
   c[2] = (double)c[2] * cosa * ept->bright;
