d2c55b7e6cd07c224845c7001355842c91412e41
Welanor <adrien.dellamaggiora@epitech.eu>
2013-04-04 19:43:49 +0200

    Add the rotation to the normal for a better light

--- rt/refraction.c
+++ rt/refraction.c
@@ -5,7 +5,7 @@
 ** Login   <dellam_a@epitech.net>
 **
 ** Started on  Mon Apr  1 12:31:29 2013 Adrien Della Maggiora
-** Last update Thu Apr  4 10:58:06 2013 Adrien Della Maggiora
+** Last update Thu Apr  4 19:29:04 2013 Adrien Della Maggiora
 */
 
 #include <math.h>
@@ -27,31 +27,23 @@ unsigned int	apply_refrac(unsigned int color1, double n, double alpha,
   return (recomp_color(c1));
 }
 
-void		get_new_vec(t_vec *vec, t_lco *lpt, double n1, double n2)
+void		get_new_vec(t_vec *res, double *nor, double *vec, double n)
 {
-  double	n;
   double	norme;
   double	cosa;
   double	sina;
 
-  if (n2 == 0)
-    return ;
-  n = n1 / n2;
-  norme = (sqrt(pow(lpt->nvec[0], 2) + pow(lpt->nvec[1], 2)
-		+ pow(lpt->nvec[2], 2))
-	   * sqrt(pow(lpt->lvec[0], 2) + pow(lpt->lvec[1], 2)));
+  norme = (sqrt(pow(nor[0], 2) + pow(nor[1], 2) + pow(nor[2], 2))
+	   * sqrt(pow(vec[0], 2) + pow(vec[1], 2)));
   if (norme < ZERO && norme > -ZERO)
     return ;
-  cosa = (lpt->nvec[0] * lpt->lvec[0]
-	  + lpt->nvec[1] * lpt->lvec[1]
-	  + lpt->nvec[2] * lpt->lvec[2]) / norme;
+  cosa = (nor[0] * vec[0] + nor[1] * vec[1] + nor[2] * vec[2]) / norme;
   sina = pow(n, 2)  * (1.0 - pow(cosa, 2));
   if (sina > 1.0)
     return ;
-  vec->vx = n * lpt->lvec[0] - (n + sqrt(1.0 - sina) * lpt->nvec[0]);
-  vec->vy = n * lpt->lvec[1] - (n + sqrt(1.0 - sina) * lpt->nvec[1]);
-  vec->vz = n * lpt->lvec[2] - (n + sqrt(1.0 - sina) * lpt->nvec[2]);
-  //printf("Incident -> %f, %f, %f\nNew Vec -> %f, %f, %f\n", lpt->lvec[0], lpt->lvec[1], lpt->lvec[2], vec->vx, vec->vy, vec->vz);
+  res->vx = n * vec[0] - (n + sqrt(1.0 - sina) * nor[0]);
+  res->vy = n * vec[1] - (n + sqrt(1.0 - sina) * nor[1]);
+  res->vz = n * vec[2] - (n + sqrt(1.0 - sina) * nor[2]);
 }
 
 unsigned int	get_new_color(t_rt *rpt, t_cam *cpt, t_lco *lpt, double n)
@@ -70,7 +62,8 @@ unsigned int	get_new_color(t_rt *rpt, t_cam *cpt, t_lco *lpt, double n)
   cpt->cx = cam_tmp.cx + k * vec_tmp.vx;
   cpt->cy = cam_tmp.cy + k * vec_tmp.vy;
   cpt->cz = cam_tmp.cz + k * vec_tmp.vz;
-  get_new_vec(&vec_tmp, lpt, n, rpt->obj[rpt->obj_num].n);
+  if (rpt->obj[rpt->obj_num].n > 0.000001)
+    get_new_vec(&vec_tmp, lpt->nvec, lpt->lvec, n / rpt->obj[rpt->obj_num].n);
   calc_inter(rpt, &k);
   cpt->cx = save.cx;
   cpt->cy = save.cy;
@@ -80,26 +73,27 @@ unsigned int	get_new_color(t_rt *rpt, t_cam *cpt, t_lco *lpt, double n)
   return (0x000000);
 }
 
-unsigned int	refrac(t_rt *rpt, t_cam *cpt, t_lco *lpt, unsigned int color)
+unsigned int	transparency(t_rt *rpt, t_cam *cpt, t_lco *lpt,
+			     unsigned int color)
 {
   double	alpha;
   double	n;
   int		obj_num;
   unsigned int	color2;
+  unsigned int	color_obj;
 
   n = 1;
+  color_obj = recomp_color(rpt->obj[rpt->obj_num].color);
   obj_num = -1;
   while (rpt->obj[rpt->obj_num].alpha > 0.0000001 && obj_num != rpt->obj_num)
     {
       obj_num = rpt->obj_num;
       color2 = get_new_color(rpt, cpt, lpt, n);
       alpha = rpt->obj[rpt->obj_num].alpha;
-      if (obj_num != rpt->obj_num)
-	{
-	  printf("Color > %X\nAlpha > %f\n", color2, alpha);
-	  color = apply_refrac(color, n, alpha, color2);
-	}
+      if (color2 != color_obj || obj_num != rpt->obj_num)
+	color = apply_refrac(color, n, alpha, color2);
       n = rpt->obj[rpt->obj_num].n;
+      obj_num = rpt->obj_num;
     }
   return (color);
 }
