c406b4e00904ee89b49c0ea286812fd1cfbfc23b
Tastyep <luc.sinet@epitech.eu>
2013-04-12 16:20:52 +0200

    separated normals and created perturbation function

--- rt/normals.c
+++ rt/normals.c
@@ -5,42 +5,54 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sun Mar 31 13:20:23 2013 luc sinet
-** Last update Fri Apr 12 11:58:18 2013 luc sinet
+** Last update Fri Apr 12 15:44:26 2013 luc sinet
 */
 
 #include <math.h>
 #include "main.h"
 #include "light.h"
 
-void	assign_normal1(t_lco *lpt, int type)
+void	perturb_normal(double *nvec, double *obj_coor, double *pert)
 {
-  if (type == 0)
-    {
-      lpt->nvec[0] = lpt->obj_coor[0];
-      lpt->nvec[1] = lpt->obj_coor[1];
-      lpt->nvec[2] = lpt->obj_coor[2];
-      lpt->nvec[1] = lpt->nvec[1] + cos(lpt->obj_coor[1] / 1.0) * (vec_norme(lpt->nvec) / 1.0);
-    }
-  else if (type == 1)
-    {
-      lpt->nvec[0] = 0;
-      lpt->nvec[1] = 0;
-      lpt->nvec[2] = 100;
-    }
+  if (pert[0] == 1)
+    nvec[0] = nvec[0] + cos(obj_coor[0] / pert[1])
+      * (vec_norme(nvec) / pert[1]);
+  if (pert[2] == 1)
+    nvec[1] = nvec[1] + cos(obj_coor[1] / pert[3])
+      * (vec_norme(nvec) / pert[3]);
+  if (pert[4] == 1)
+    nvec[2] = nvec[2] + cos(obj_coor[2] / pert[5])
+      * (vec_norme(nvec) / pert[5]);
 }
 
-void	assign_normal2(t_lco *lpt, int type)
+void	sphere_normal(double *nvec, double *obj_coor, double *pert)
 {
-  if (type == 2)
-    {
-      lpt->nvec[0] = lpt->obj_coor[0];
-      lpt->nvec[1] = lpt->obj_coor[1];
-      lpt->nvec[2] = -0.6 * lpt->obj_coor[2];
-    }
-  else if (type == 3)
-    {
-      lpt->nvec[0] = lpt->obj_coor[0];
-      lpt->nvec[1] = lpt->obj_coor[1];
-      lpt->nvec[2] = 0;
-    }
+  nvec[0] = obj_coor[0];
+  nvec[1] = obj_coor[1];
+  nvec[2] = obj_coor[2];
+  perturb_normal(nvec, obj_coor, pert);
+}
+
+void	plan_normal(double *nvec, double *obj_coor, double *pert)
+{
+  nvec[0] = 0;
+  nvec[1] = 0;
+  nvec[2] = 100;
+  perturb_normal(nvec, obj_coor, pert);
+}
+
+void	cone_normal(double *nvec, double *obj_coor, double *pert)
+{
+  nvec[0] = obj_coor[0];
+  nvec[1] = obj_coor[1];
+  nvec[2] = -0.6 * obj_coor[2];
+  perturb_normal(nvec, obj_coor, pert);
+}
+
+void	cylinder_normal(double *nvec, double *obj_coor, double *pert)
+{
+  nvec[0] = obj_coor[0];
+  nvec[1] = obj_coor[1];
+  nvec[2] = 0;
+  perturb_normal(nvec, obj_coor, pert);
 }
