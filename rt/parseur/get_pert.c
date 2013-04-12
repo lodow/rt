e9ee7b6d39688263807f6b3b405fe479149ff946
Tastyep <luc.sinet@epitech.eu>
2013-04-12 17:40:37 +0200

    added the fill getting perturbations

---
+++ rt/parseur/get_pert.c
@@ -0,0 +1,50 @@
+/*
+** get_pert.c for pert in /home/sinet_l//rt/rt/parseur
+**
+** Made by luc sinet
+** Login   <sinet_l@epitech.net>
+**
+** Started on  Fri Apr 12 16:59:39 2013 luc sinet
+** Last update Fri Apr 12 17:34:41 2013 luc sinet
+*/
+
+#include "../include/main.h"
+#include "../include/pars.h"
+#include "../include/nb.h"
+
+void	check_space_arg(char *line, double *pert, int *i)
+{
+  int	x;
+
+  pert[0] = 1;
+  if (line[0] == ':')
+    pert[1] = my_fgetnbr(&line[1]);
+  else
+    return ;
+  *i += 1;
+  x = 1;
+  while (line[x] >= '0' && line[x] <= '9')
+    {
+      ++(*i);
+      ++x;
+    }
+}
+
+void	fill_pert(char *line, double *pert)
+{
+  int	i;
+  int	x;
+
+  i = 0;
+  x = 0;
+  while (line[i] && x < 3)
+    {
+      skip_adds(line, &i);
+      if (line[i] == 'x' || line[i] == 'y' || line[i] == 'z')
+	check_space_arg(&line[i + 1], &pert[x], &i);
+      else
+	return ;
+      ++x;
+      skip_adds(line, &i);
+    }
+}
