4b8ab6ff699ee9e40276d8507bbed4d107c88de9
Adrien <adrien.dellamaggiora@epitech.eu>
2013-04-17 16:18:41 +0200

    Add rpt->obj[obj].pos to cam.

--- rt/shadows.c
+++ rt/shadows.c
@@ -5,7 +5,7 @@
 ** Login   <dellam_a@epitech.net>
 **
 ** Started on  Tue Apr  9 10:14:18 2013 Adrien Della Maggiora
-** Last update Wed Apr 17 15:35:55 2013 luc sinet
+** Last update Wed Apr 17 16:09:06 2013 Adrien
 */
 
 #include <math.h>
@@ -21,9 +21,12 @@ int		shadows(t_rt *rpt, t_cam *cpt, t_lig *spot, t_lco *lpt)
   obj = rpt->obj_num;
   copy_tab(cpt->pos, cam, 3);
   copy_tab(lpt->obj_coor, cpt->pos, 3);
-  rpt->vpt->vec[0] = spot->pos[0] - lpt->obj_coor[0];
-  rpt->vpt->vec[1] = spot->pos[1] - lpt->obj_coor[1];
-  rpt->vpt->vec[2] = spot->pos[2] - lpt->obj_coor[2];
+  cpt->pos[0] += rpt->obj[obj].pos[0];
+  cpt->pos[1] += rpt->obj[obj].pos[1];
+  cpt->pos[2] += rpt->obj[obj].pos[2];
+  rpt->vpt->vec[0] = spot->pos[0] - cpt->pos[0];
+  rpt->vpt->vec[1] = spot->pos[1] - cpt->pos[1];
+  rpt->vpt->vec[2] = spot->pos[2] - cpt->pos[2];
   calc_inter(rpt, &k);
   copy_tab(cam, cpt->pos, 3);
   if (rpt->obj_num != obj && (k > ZERO && k < 1))
