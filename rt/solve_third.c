f16675dd642c375e5c1815664dee0c3c199c2c1c
Tastyep <luc.sinet@epitech.eu>
2013-04-22 21:15:56 +0200

    added pq function

--- rt/solve_third.c
+++ rt/solve_third.c
@@ -5,6 +5,14 @@
 ** Login   <debas_e@epitech.net>
 **
 ** Started on  Mon Apr 22 21:01:11 2013 etienne debas
-** Last update Mon Apr 22 21:01:13 2013 etienne debas
+** Last update Mon Apr 22 21:15:35 2013 luc sinet
 */
 
+void	get_pq(double *coef, double *pq)
+{
+  pq[0] = (-pow(coef[1], 2)) / (3.0 * pow(coef[0], 2))
+    + coef[2] / coef[0];
+  pq[1] = (coef[1] / (27.0 * coef[0]))
+    * (((2.0 * pow(coef[1], 2)) / pow(coef[0], 2))
+       - ((9.0 * coef[2]) / coef[0])) + (coef[3] / coef[0]);
+}
