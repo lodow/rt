d9c95ff3686195db12faa563b059451345596e75
Etienne DEBAS <etienne.debas@epitech.eu>
2013-04-22 21:00:13 +0200

    add coef moebius

--- rt/solve_equation.c
+++ rt/solve_equation.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Fri Mar 29 10:47:30 2013 luc sinet
-** Last update Fri Mar 29 13:32:55 2013 luc sinet
+** Last update Mon Apr 22 20:54:09 2013 etienne debas
 */
 
 void	fourth(double *c, double *p)
@@ -25,6 +25,28 @@ void	x_res(double *p, double *x)
   x[1] = sqrt((p[0] / 2.0) - sqrt((pos(p[0], 2) / 4.0) - p[2]));
   x[2] = -sqrt((p[0] / 2.0) + sqrt((pos(p[0], 2) / 4.0) - p[2]));
   x[3] = -sqrt((p[0] / 2.0) - sqrt((pos(p[0], 2) / 4.0) - p[2]));
+}
+
+void		mk_coef_moebius(double *cam, double *vec, double *ept)
+{
+  double	k[2];
+  double	a[4];
+
+  a[0] = (pow(vec[0], 2) * vec[1] + pow(vec[2], 2) * vec[1] + pow(vec[1], 3)
+	  - 2 * pow(vec[0], 2) * vec[2] - 2 * pos(vec[1], 2) * vec[2]);
+  a[1] = (pow(vec[0], 2) * cam[1] + 2 * cam[0] * vec[0] * vec[1] + pow(vec[2], 2)
+	  * cam[1] + 2 * cam[2] * vec[1] * vec[2] + pow(vec[2], 2) * cam[1]
+	  + 2 * cam[1] * pow(vec[1], 2) - 2 * vec[0] * vec[2] - 2 * pow(vec[2], 2)
+	  * cam[2] - 4 * cam[0] * vec[0] * vec[2] - 2 * pow(vec[1], 2) * cam[2]
+	  - 4 * cam[1] * vec[1] * vec[2]);
+  a[2] = (2 * cam[0] * vec[0] * cam[1] + cam[0] * vec[1] + 2 * cam[2] * vec[2] * cam[1]
+	  + pow(cam[2], 2) * vec[1] + 2 * pow(cam[1], 2) * vec[1] + vec[1] * pow(cam[1], 2)
+	  + vec[1] - 2 * cam[0] * vec[2] - 2 * vec[0] * cam[2] - 4
+	  * cam[0] * vec[0] * cam[2] - 2 * pow(cam[0], 2) * vec[2] - 4
+	  * cam[1] * vec[1] * cam[2] - 2 * pow(cam[1], 2) * vec[2]);
+  a[3] = (pow(cam[0], 2) * cam[1] + pow(cam[2], 2) * cam[1]
+	  + pow(cam[1], 3) - cam[1] - 2 * cam[0] * cam[2]
+	  - 2 * pow(cam[0], 2) * cam[2] - 2 * pow(cam[1], 2) * cam[2]):
 }
 
 void	new_res(double *p, double *x)
