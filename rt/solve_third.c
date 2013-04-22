ff0349260bddac972c489ab869466dc197df21ec
Etienne DEBAS <etienne.debas@epitech.eu>
2013-04-22 21:01:33 +0200

    add solve third

---
+++ rt/solve_third.c
@@ -0,0 +1,10 @@
+/*
+** solve_third.c for rt in /tmp/rt/rt
+**
+** Made by etienne debas
+** Login   <debas_e@epitech.net>
+**
+** Started on  Mon Apr 22 21:01:11 2013 etienne debas
+** Last update Mon Apr 22 21:01:13 2013 etienne debas
+*/
+
