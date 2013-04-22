f628f4a17c04992e3ac3c917aa2ff2e96f3ee300
Etienne DEBAS <etienne.debas@epitech.eu>
2013-04-22 23:35:00 +0200

    add resolve third

---
+++ rt/inter_third.c
@@ -0,0 +1,34 @@
+/*
+** inter_third.c for rt in /tmp/rt/rt
+**
+** Made by etienne debas
+** Login   <debas_e@epitech.net>
+**
+** Started on  Mon Apr 22 23:06:51 2013 etienne debas
+** Last update Mon Apr 22 23:32:26 2013 etienne debas
+*/
+
+#include <math.h>
+
+void		mk_coef_moebius(double *cam, double *vec, double *ept)
+{
+  double	k[2];
+  double	a[4];
+
+  a[0] = (pow(vec[0], 2) * vec[1] + pow(vec[2], 2) * vec[1] + pow(vec[1], 3)
+	  - 2 * pow(vec[0], 2) * vec[2] - 2 * pow(vec[1], 2) * vec[2]);
+  a[1] = (pow(vec[0], 2) * cam[1] + 2 * cam[0] * vec[0] * vec[1] + pow(vec[2], 2)
+	  * cam[1] + 2 * cam[2] * vec[1] * vec[2] + pow(vec[2], 2) * cam[1]
+	  + 2 * cam[1] * pow(vec[1], 2) - 2 * vec[0] * vec[2] - 2 * pow(vec[2], 2)
+	  * cam[2] - 4 * cam[0] * vec[0] * vec[2] - 2 * pow(vec[1], 2) * cam[2]
+	  - 4 * cam[1] * vec[1] * vec[2]);
+  a[2] = (2 * cam[0] * vec[0] * cam[1] + cam[0] * vec[1] + 2 * cam[2] * vec[2] * cam[1]
+	  + pow(cam[2], 2) * vec[1] + 2 * pow(cam[1], 2) * vec[1] + vec[1] * pow(cam[1], 2)
+	  + vec[1] - 2 * cam[0] * vec[2] - 2 * vec[0] * cam[2] - 4
+	  * cam[0] * vec[0] * cam[2] - 2 * pow(cam[0], 2) * vec[2] - 4
+	  * cam[1] * vec[1] * cam[2] - 2 * pow(cam[1], 2) * vec[2]);
+  a[3] = (pow(cam[0], 2) * cam[1] + pow(cam[2], 2) * cam[1]
+	  + pow(cam[1], 3) - cam[1] - 2 * cam[0] * cam[2]
+	  - 2 * pow(cam[0], 2) * cam[2] - 2 * pow(cam[1], 2) * cam[2]);
+  return (solve_third(a));
+}
--- rt/solve_third.c
+++ rt/solve_third.c
@@ -5,6 +5,59 @@
 ** Login   <debas_e@epitech.net>
 **
 ** Started on  Mon Apr 22 21:01:11 2013 etienne debas
-** Last update Mon Apr 22 21:01:13 2013 etienne debas
+** Last update Mon Apr 22 23:32:48 2013 etienne debas
 */
 
+#include <math.h>
+#include "include/main.h"
+
+double		solve_third(double *coef)
+{
+  double	pq[2];
+  double	delta;
+  double	k[3];
+
+  get_pq(coef, &pq);
+  k[0] = -1;
+  k[1] = -1;
+  k[2] = -1;
+  delta = pow(pq[1], 2) + ((4 * pow(pq[0], 3)) / 27);
+  if (delta < 0)
+    delta_neg_third(k, delta, pq);
+  else if (delta == 0.0 && pq[0] == 0.0 && pq[1] == 0.0)
+    k[0] = 0;
+  else if (delta == 0.0)
+    delta_nul_third(k, pq);
+  else if (delta > 0.0)
+    delta_pos_third(k, delta, pq);
+  return (get_min(k, 3));
+}
+
+void		delta_neg_third(double *k, double delta, double *pq)
+{
+  double	u;
+  double	v;
+
+  u = pow(-(pq[1] + sqrt(delta)) / 2, 1 / 3);
+  v = pow(-(pq[1] - sqrt(delta)) / 2, 1 / 3);
+  k[0] = u + v;
+}
+
+void		delta_nul_third(double *k, double *pq)
+{
+  k[0] = (3 * pq[1]) / pq[0];
+  k[1] = (-3 * pq[1]) / (2 * pq[0]);
+}
+
+void		delta_pos_third(double *k, double delta, double *pq)
+{
+  int		i;
+
+  i = 0;
+  while (i < 3)
+    {
+      k[i] = 2 * sqrt(-pq[0] / 3)
+	* cos((1 / 3) * acos((-pq[1] / 2) * sqrt(27 / pow(-pq[0], 3))) + (2 * i * M_PI) / 3);
+      i++;
+    }
+}
