9f202b96f033ecf9e9b0f2a4351b706af31672c4
Tastyep <luc.sinet@epitech.eu>
2013-05-01 18:41:17 +0200

    limit depth works alone too

--- rt/limit_depth.c
+++ rt/limit_depth.c
@@ -5,48 +5,65 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Wed Apr 10 22:20:26 2013 luc sinet
-** Last update Sun Apr 14 12:39:55 2013 luc sinet
+** Last update Wed May  1 18:37:15 2013 luc sinet
 */
 
 #include "include/main.h"
 
 double		test_forw_limit(double *cam, double *vec,
-			      double k, double *limit)
+			      double *k, double *limit)
 {
+  double       	mk[2];
   double	inter[3];
 
-  get_inter(cam, vec, k, inter);
-  if (inter[0] < limit[7])
-    return (-1);
-  if (limit[8] != IVAL && inter[0] > limit[8])
-    return (-1);
-  return (k);
+  mk[0] = get_min(k, 2);
+  get_inter(cam, vec, mk[0], inter);
+  if (inter[0] < limit[1])
+    {
+      mk[1] = get_max(k, 2);
+      get_inter(cam, vec, mk[1], inter);
+      if (inter[0] < limit[1])
+	return (-1);
+      else
+	return (mk[1]);
+    }
+  return (mk[0]);
 }
 
 double		test_back_limit(double *cam, double *vec,
-				double k, double *limit)
+				double *k, double *limit)
 {
+
+  double       	mk[2];
   double	inter[3];
 
-  get_inter(cam, vec, k, inter);
-  if (inter[0] > limit[8])
-    return (-1);
-  if (limit[7] != IVAL && inter[0] < limit[7])
-    return (-1);
-  return (k);
+  mk[0] = get_min(k, 2);
+  get_inter(cam, vec, mk[0], inter);
+  if (inter[0] > limit[2])
+    {
+      mk[1] = get_max(k, 2);
+      get_inter(cam, vec, mk[1], inter);
+      if (inter[0] > limit[2])
+	return (-1);
+      else
+	return (mk[1]);
+    }
+  return (mk[0]);
 }
 
 double		test_depth_limit(double *cam, double *vec,
 				 double *limit, double *k)
 {
   double	min;
-  double	inter[3];
+  double	current;
 
   min = get_min(k, 2);
-  get_inter(cam, vec, min, inter);
-  if (limit[7] != IVAL && inter[0] < limit[7])
-    return (test_forw_limit(cam, vec, get_max(k, 2), limit));
-  else if (limit[8] != IVAL && inter[0] > limit[8])
-    return (test_back_limit(cam, vec, get_max(k, 2), limit));
+  current = min;
+  if (limit[1] != IVAL)
+    current = test_forw_limit(cam, vec, k, limit);
+  min = GMAX(min, current);
+  if (min >= 0 && limit[2] != IVAL)
+    current = test_back_limit(cam, vec, k, limit);
+  min = GMAX(min, current);
   return (min);
 }
