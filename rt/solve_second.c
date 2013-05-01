a83e65312552218b75415c179a539cb5e5bcfeeb
Tastyep <luc.sinet@epitech.eu>
2013-05-01 17:39:51 +0200

    get_max function was false

--- rt/solve_second.c
+++ rt/solve_second.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Tue Apr  9 14:49:14 2013 luc sinet
-** Last update Mon Apr 22 21:20:53 2013 luc sinet
+** Last update Wed May  1 17:39:04 2013 luc sinet
 */
 
 #include <math.h>
@@ -27,7 +27,7 @@ double		get_max(double *k, int size)
   max = k[0];
   while (i < size)
     {
-      if (k[i] > size)
+      if (k[i] > max)
 	max = k[i];
       ++i;
     }
@@ -43,7 +43,7 @@ double		get_min(double *k, int size)
   smallest = k[0];
   while (i < size)
     {
-      if ((k[i] >= ZERO && k[i] < smallest) || smallest < ZERO)
+      if ((k[i] > ZERO && k[i] < smallest) || smallest < ZERO)
 	smallest = k[i];
       ++i;
     }
