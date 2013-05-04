1a807a7858a695c77f1fb46642a8f56f1e63553f
Tastyep <luc.sinet@epitech.eu>
2013-05-04 20:37:50 +0200

    added more explicit messages and a function like merror for parsing

--- rt/parseur/error.c
+++ rt/parseur/error.c
@@ -5,10 +5,11 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Mon Feb 25 11:02:44 2013 luc sinet
-** Last update Sat May  4 11:20:13 2013 luc sinet
+** Last update Sat May  4 20:35:25 2013 luc sinet
 */
 
 #include "../include/main.h"
+#include "../include/pars.h"
 #include "../include/str.h"
 
 int	merror(char *error, int val)
@@ -19,10 +20,14 @@ int	merror(char *error, int val)
 
 int	file_error(char *line, int lnb, int ret)
 {
-  my_putstr("l ", 2);
-  my_put_nbr(lnb, 2);
-  my_putstr(" :", 2);
-  my_putstr(line, 2);
-  my_putstr("\nis invalid\n", 2);
+  int	i;
+
+  i = 0;
+  skip_adds(line, &i);
+  my_putstr("line ", 2);
+  my_put_nbr(lnb + 1, 2);
+  my_putstr(": \"", 2);
+  my_putstr(&line[i], 2);
+  my_putstr("\" is invalid\n", 2);
   return (ret);
 }
