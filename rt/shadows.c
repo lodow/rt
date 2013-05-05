18790873b342675e2f505d2740b27ffdf3ab2c09
Tastyep <luc.sinet@epitech.eu>
2013-05-05 10:51:41 +0200

    useless lines are useless

--- rt/shadows.c
+++ rt/shadows.c
@@ -5,7 +5,7 @@
 ** Login   <dellam_a@epitech.net>
 **
 ** Started on  Tue Apr  9 10:14:18 2013 Adrien Della Maggiora
-** Last update Fri May  3 16:06:53 2013 luc sinet
+** Last update Sun May  5 10:51:21 2013 luc sinet
 */
 
 #include <math.h>
@@ -39,9 +39,6 @@ double		shadows(t_rt *rpt, double *cpos, double *lpos, double *opos)
   copy_tab(cpos, cam, 3);
   copy_tab(vpos, vec, 3);
   copy_tab(opos, cpos, 3);
-  cpos[0] = opos[0];
-  cpos[1] = opos[1];
-  cpos[2] = opos[2];
   vpos[0] = lpos[0] - cpos[0];
   vpos[1] = lpos[1] - cpos[1];
   vpos[2] = lpos[2] - cpos[2];
