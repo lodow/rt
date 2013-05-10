e8af83d161cf1ce93defce69db85018331f78c1c
Adrien <adrien.dellamaggiora@epitech.eu>
2013-05-10 18:56:04 +0200

    We have now a real transparency.

--- rt/transparency.c
+++ rt/transparency.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sun May  5 18:27:59 2013 luc sinet
-** Last update Thu May  9 10:30:30 2013 Adrien Della Maggiora
+** Last update Fri May 10 18:51:53 2013 Adrien Della Maggiora
 */
 
 #include <math.h>
@@ -13,35 +13,59 @@
 #include "light.h"
 #include "change_color.h"
 
+unsigned int	calc_trans(unsigned int *all_color, double *alpha, int i,
+			   unsigned int color)
+{
+  unsigned int	res;
+
+  printf("-- Start --\n");
+  printf("%X\n", all_color[i]);
+  res = all_color[i--];
+  while (i >= 0)
+    {
+      printf("%X\n", all_color[i]);
+      res = apply_indice(all_color[i], res, alpha[i + 1]);
+      --i;
+    }
+  res = apply_indice(color, res, alpha[0]);
+  printf("%X\n", color);
+  printf("\n%X\n", res);
+  printf("-- END --\n");
+  return (res);
+}
+
 unsigned int    transparency(t_rt *rpt, t_lco *lpt, unsigned int color,
 			     double k)
 {
   int		obj;
+  int		i;
   int		save_obj;
   double	n;
-  double	alpha;
   double	vec[3];
+  double	alpha[MAX_R];
+  unsigned int	all_color[MAX_R];
 
   n = 1;
   save_obj = obj = rpt->obj_num;
   copy_tab(rpt->vpt->vec, vec, 3);
   copy_tab(rpt->cpt->pos, rpt->cpt->ctmp, 3);
-  while (rpt->obj[rpt->obj_num].indice[0] > ZERO
-	 && rpt->obj[rpt->obj_num].indice[1] > ZERO && k > ZERO)
+  i = 0;
+  while (i < MAX_R && rpt->obj[rpt->obj_num].indice[0] > ZERO && n > ZERO
+	 && k > ZERO)
     {
       obj = rpt->obj_num;
-      alpha = rpt->obj[obj].indice[0];
+      alpha[i] = rpt->obj[obj].indice[0];
       calc_refrac(rpt, rpt->vpt, lpt, k, rpt->obj[obj].indice[1] / n);
       n = rpt->obj[obj].indice[1];
       calc_inter(rpt, &k);
       if (k > ZERO && rpt->obj_num != obj)
-	color = apply_indice(color, get_light(rpt, k, &rpt->obj[rpt->obj_num],
-					      lpt), alpha);
+	all_color[i] = get_light(rpt, k, &rpt->obj[rpt->obj_num], lpt);
       else if (k == -1)
-	color = apply_indice(color, 0x000000, alpha);
+	all_color[i] = 0x000000;
+      ++i;
     }
   copy_tab(rpt->cpt->ctmp, rpt->cpt->pos, 3);
   copy_tab(vec, rpt->vpt->vec, 3);
   rpt->obj_num = save_obj;
-  return (color);
+  return ((i > 0) ? calc_trans(all_color, alpha, i - 1, color) : color);
 }
