6df5a7c88c1eef874f8537f46cc7500e3b4ac3e7
Adrien <adrien.dellamaggiora@epitech.eu>
2013-05-11 14:43:34 +0200

    Resolve the uninitialized value in tranparency.

--- rt/transparency.c
+++ rt/transparency.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sun May  5 18:27:59 2013 luc sinet
-** Last update Fri May 10 18:58:31 2013 Adrien Della Maggiora
+** Last update Sat May 11 14:42:17 2013 Adrien Della Maggiora
 */
 
 #include <math.h>
@@ -53,10 +53,9 @@ unsigned int    transparency(t_rt *rpt, t_lco *lpt, unsigned int color,
       n = rpt->obj[obj].indice[1];
       calc_inter(rpt, &k);
       if (k > ZERO && rpt->obj_num != obj)
-	all_color[i] = get_light(rpt, k, &rpt->obj[rpt->obj_num], lpt);
+	all_color[i++] = get_light(rpt, k, &rpt->obj[rpt->obj_num], lpt);
       else if (k == -1)
-	all_color[i] = 0x000000;
-      ++i;
+	all_color[i++]  = 0x000000;
     }
   copy_tab(rpt->cpt->ctmp, rpt->cpt->pos, 3);
   copy_tab(vec, rpt->vpt->vec, 3);
