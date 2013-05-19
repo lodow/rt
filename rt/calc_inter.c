662c4ef3e3e713f36e79b733d0870b94eb981ebf
Tastyep <luc.sinet@epitech.eu>
2013-05-19 22:43:18 +0200

    maked place in calc_inter to make the main block

--- rt/calc_inter.c
+++ rt/calc_inter.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Wed Mar 20 16:55:47 2013 luc sinet
-** Last update Sun May 19 00:29:21 2013 etienne debas
+** Last update Sun May 19 22:42:23 2013 luc sinet
 */
 
 #include <math.h>
@@ -14,21 +14,6 @@
 #include "change_color.h"
 #include "light.h"
 #include "supersampling.h"
-
-void	assign_function(t_rt *rpt)
-{
-  rpt->eptr[0] = &sphere;
-  rpt->eptr[1] = &plan;
-  rpt->eptr[2] = &cone;
-  rpt->eptr[3] = &cylinder;
-  rpt->eptr[4] = &circle;
-  rpt->eptr[5] = &square;
-  rpt->eptr[6] = &triangle;
-  rpt->eptr[7] = &paraboloide;
-  rpt->eptr[8] = &hyperboloide;
-  rpt->eptr[9] = &moebius;
-  rpt->eptr[10] = &tangle;
-}
 
 void		calc_inter(t_rt *rpt, double *kmin)
 {
@@ -47,14 +32,6 @@ void		calc_inter(t_rt *rpt, double *kmin)
 	}
       ++i;
     }
-}
-
-void		new_coor(t_vec *vpt, t_cam *cpt, double x, double y)
-{
-  vpt->vec[0] = DIST - cpt->pos[0];
-  vpt->vec[1] = (WINX / 2.0 - x) - cpt->pos[1];
-  vpt->vec[2] = (WINY / 2.0 - y) - cpt->pos[2];
-  rotate(vpt->vec, cpt->ccos, cpt->csin, 0);
 }
 
 unsigned int	get_pixel_color(t_rt *rpt, int *pos)
