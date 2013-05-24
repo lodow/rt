fb27b7a2493c7e39e454e593dc1fed9ec4ed3e15
Tastyep <luc.sinet@epitech.eu>
2013-05-24 17:37:20 +0200

    added option to specifie the name of the bmp output

--- rt/main.c
+++ rt/main.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Mon Feb 25 10:36:59 2013 luc sinet
-** Last update Thu May 23 17:16:58 2013 Adrien Della Maggiora
+** Last update Fri May 24 17:33:16 2013 luc sinet
 */
 
 #include "main.h"
@@ -85,7 +85,8 @@ int		main(int ac, char **av)
     return (-1);
   init_cos(rpt.obj);
   calc_pixel(&rpt, &cpt, &vpt, &ppt);
-  output_bmp(&ppt);
+  if (ac == 3)
+    output_bmp(&ppt, av[2]);
   free_all(&rpt, &ppt);
   mlx_key_hook(ppt.win_ptr, key_event, &rpt);
   mlx_expose_hook(ppt.win_ptr, print_i, &ppt);
