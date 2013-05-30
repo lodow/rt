4898542b85313c713b29325330c7322ac64d815c
Tastyep <luc.sinet@epitech.eu>
2013-05-30 16:49:55 +0200

    fixed bug on supersampling close #4

--- rt/supersampling.c
+++ rt/supersampling.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Fri Apr  5 11:07:42 2013 luc sinet
-** Last update Mon May 20 16:30:23 2013 luc sinet
+** Last update Thu May 30 16:48:25 2013 luc sinet
 */
 
 #include <math.h>
@@ -15,43 +15,27 @@
 
 unsigned int	mix_color(unsigned int *tab, int ssp)
 {
-  unsigned char	final_comp[3];
+  double	final_comp[3];
   unsigned char	temp_comp[3];
+  unsigned char	final[3];
   int		i;
 
-  i = 1;
-  decomp_color(tab[0], final_comp);
+  i = 0;
+  final_comp[0] = 0;
+  final_comp[1] = 0;
+  final_comp[2] = 0;
   while (i < ssp)
     {
       decomp_color(tab[i], temp_comp);
-      final_comp[0] = (final_comp[0] + temp_comp[0]) / 2.0;
-      final_comp[1] = (final_comp[1] + temp_comp[1]) / 2.0;
-      final_comp[2] = (final_comp[2] + temp_comp[2]) / 2.0;
+      final_comp[0] += ((double)temp_comp[0] / ssp);
+      final_comp[1] += ((double)temp_comp[1] / ssp);
+      final_comp[2] += ((double)temp_comp[2] / ssp);
       ++i;
     }
-  return (recomp_color(final_comp));
-}
-
-int	get_int(double space, int *virg)
-{
-
-  *virg = 0;
-  while ((space - (int)space) > ZERO && *virg < 10)
-    {
-      ++(*virg);
-      space *= 10;
-    }
-  return (space);
-}
-
-void	get_rand_pos(double *pos, double *new_pos, double spacing)
-{
-  int	virg;
-  int	nspace;
-
-  nspace = get_int(spacing, &virg);
-  new_pos[0] = pos[0] + (double)((rand() % nspace)) / (double)pow(10, virg);
-  new_pos[1] = pos[1] + (double)((rand() % nspace)) / (double)pow(10, virg);
+  final[0] = final_comp[0];
+  final[1] = final_comp[1];
+  final[2] = final_comp[2];
+  return (recomp_color(final));
 }
 
 unsigned int   	supersampling(t_rt *rpt, t_samp *spt, int x, int y)
@@ -71,10 +55,8 @@ unsigned int   	supersampling(t_rt *rpt, t_samp *spt, int x, int y)
       pos[0] = x;
       while (i < ssp && pos[0] < x + 1)
 	{
-	  /* get_rand_pos(pos, r_pos, spacing); */
 	  new_coor(rpt->vpt, rpt->cpt, pos[0], pos[1]);
 	  spt->pixel[i] = get_pixel_color(rpt);
-	  /* printf("y %f  x %f\n", r_pos[1], r_pos[0]); */
 	  pos[0] += spacing;
 	  ++i;
 	}
