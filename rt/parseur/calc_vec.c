443716dd98ec41347e172816e26078b04f857f92
izissise <hugues.m@hotmail.fr>
2013-06-01 19:02:04 +0200

    Modify the scene to see that (normals ?) not working.

--- rt/parseur/calc_vec.c
+++ rt/parseur/calc_vec.c
@@ -18,9 +18,9 @@ void	calc_vec(double *point, double *normal, t_obj *obj)
   obj->v2[0] = (point[6] - point[3]);
   obj->v2[1] = (point[7] - point[4]);
   obj->v2[2] = (point[8] - point[5]);
-  obj->pos[0] = point[0];
-  obj->pos[1] = point[1];
-  obj->pos[2] = point[2];
+  obj->pos[0] += point[3];
+  obj->pos[1] += point[4];
+  obj->pos[2] += point[5];
   obj->normal[0] = normal[0];
   obj->normal[1] = normal[1];
   obj->normal[2] = normal[2];
