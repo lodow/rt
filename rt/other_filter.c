e07cf10b3633230a844ed44be0db2045d484e989
Tastyep <luc.sinet@epitech.eu>
2013-06-02 12:46:43 +0200

    forgot this unused

--- rt/other_filter.c
+++ rt/other_filter.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Tue May 28 13:44:12 2013 luc sinet
-** Last update Wed May 29 19:34:34 2013 luc sinet
+** Last update Sun Jun  2 12:46:07 2013 luc sinet
 */
 
 #include "main.h"
@@ -16,6 +16,7 @@ unsigned int	revers_filter(unsigned int color, t_opt *opt)
 {
   unsigned char	comp[3];
 
+  (void)opt;
   decomp_color(color, comp);
   comp[0] = LIMIT(2.0 * (128 - comp[0]) + comp[0], 0, 255);
   comp[1] = LIMIT(2.0 * (128 - comp[1]) + comp[1], 0, 255);
