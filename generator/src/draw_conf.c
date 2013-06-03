ccc22788a7057db3a34405a953876fe6796a64cf
remirobert <remi.robert@epitech.eu>
2013-06-03 12:57:59 +0200

    add generical function fr draw UI textbox

--- generator/src/draw_conf.c
+++ generator/src/draw_conf.c
@@ -5,15 +5,23 @@
 ** Login   <robert_r@epitech.net>
 **
 ** Started on  Mon Jun  3 12:44:40 2013 remi robert
-** Last update Mon Jun  3 12:45:24 2013 remi robert
+** Last update Mon Jun  3 12:56:54 2013 remi robert
 */
 
 #include "my_func.h"
 
+void	draw_conf_box(t_param *param, int x, int y, char *str)
+{
+  mlx_string_put(param->window.p, param->window.id, x - 150, y + 20,
+		 0xFFFFFFFF, str);
+  mlx_put_image_to_window(param->window.p, param->window.id,
+			  param->textbox.img, x, y);
+}
+
 void	draw_conf(t_param *param)
 {
-  mlx_string_put(param->window.p, param->window.id, 450, 50,
-		 0xFFFFFFFF, "conf z : ");
-  mlx_put_image_to_window(param->window.p, param->window.id,
-			  param->textbox.img, 450, 60);
+  draw_conf_box(param, 450, 60, "conf z :");
+  draw_conf_box(param, 450, 100, "color :");
+  draw_conf_box(param, 450, 140, "conf z :");
+  draw_conf_box(param, 450, 180, "conf z :");
 }
