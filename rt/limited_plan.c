be4d50b729a1a89f5aeff5093bc0b0769a2da8f6
Adrien <dmadrien@msn.com>
2013-06-08 01:26:36 +0200

    Norme limited_plan

--- rt/limited_plan.c
+++ rt/limited_plan.c
@@ -5,7 +5,7 @@
 ** Login   <adrien@Adrien>
 **
 ** Started on  Sat May  4 14:46:20 2013 Adrien
-** Last update Fri Jun  7 23:36:42 2013 adrien dellamaggiora
+** Last update Sat Jun  8 01:25:48 2013 adrien dellamaggiora
 */
 
 #include "main.h"
@@ -50,8 +50,6 @@ double		square(double *cam, double *vec, t_obj *ept)
 double		triangle(double *cam, double *vec, t_obj *ept)
 {
   double	pt[3];
-  double	u[3];
-  double	v[3];
   double	w[3];
   double	d;
   double	ret;
@@ -63,12 +61,6 @@ double		triangle(double *cam, double *vec, t_obj *ept)
   pt[0] = vec[0] * ret + cam[0];
   pt[1] = vec[1] * ret + cam[1];
   pt[2] = vec[2] * ret + cam[2];
-  /* v[0] = ept->pt2[0] - ept->pos[0]; */
-  /* v[1] = ept->pt2[1] - ept->pos[1]; */
-  /* v[2] = ept->pt2[2] - ept->pos[2]; */
-  /* u[0] = ept->pt1[0] - ept->pos[0]; */
-  /* u[1] = ept->pt1[1] - ept->pos[1]; */
-  /* u[2] = ept->pt1[2] - ept->pos[2]; */
   w[0] = pt[0] - ept->pos[0];
   w[1] = pt[1] - ept->pos[1];
   w[2] = pt[2] - ept->pos[2];
