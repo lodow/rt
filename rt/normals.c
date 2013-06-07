3e352914febc08b31620abff764b13a7ebda02c8
Etienne DEBAS <etienne.debas@epitech.eu>
2013-06-07 19:57:35 +0200

    solve norme error

--- rt/normals.c
+++ rt/normals.c
@@ -5,7 +5,7 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Sun Mar 31 13:20:23 2013 luc sinet
-** Last update Fri Jun  7 19:08:32 2013 etienne debas
+** Last update Fri Jun  7 19:54:27 2013 etienne debas
 */
 
 #include <math.h>
@@ -60,37 +60,5 @@ void	cylinder_normal(double *nvec, double *obj_coor, double *pert,
   nvec[0] = obj_coor[0];
   nvec[1] = obj_coor[1];
   nvec[2] = 0;
-  perturb_normal(nvec, obj_coor, pert);
-}
-
-void	moebius_normal(double *nvec, double *obj_coor,
-                     double *pert, UNUSED t_obj *obj)
-{
-  nvec[0] = obj_coor[0];
-  nvec[1] = obj_coor[1];
-  nvec[2] = obj_coor[2];
-  perturb_normal(nvec, obj_coor, pert);
-}
-
-void	tangle_normal(double *nvec, double *obj_coor, double *pert, t_obj *obj)
-{
-  nvec[0] = 4 * pow(obj_coor[0], 3) - 20 * pow(obj->size[0], 2) * obj_coor[0];
-  nvec[1] = 4 * pow(obj_coor[1], 3) - 20 * pow(obj->size[0], 2) * obj_coor[1];
-  nvec[2] = 4 * pow(obj_coor[2], 3) - 20 * pow(obj->size[0], 2) * obj_coor[2];
-  perturb_normal(nvec, obj_coor, pert);
-}
-
-void		tore_normal(double *nvec, double *obj_coor, double *pert, t_obj *obj)
-{
-  double	k;
-
-  k = 4 * (pow(obj_coor[0], 2)
-           + pow(obj_coor[1], 2)
-           + pow(obj_coor[2], 2)
-           + pow(obj->size[0], 2)
-           - pow(obj->size[1], 2));
-  nvec[0] = obj_coor[0] * k - 8.0 * pow(obj->size[0], 2) * obj_coor[0];
-  nvec[1] = obj_coor[1] * k - 8.0 * pow(obj->size[0], 2) * obj_coor[1];
-  nvec[2] = obj_coor[2] * k;
   perturb_normal(nvec, obj_coor, pert);
 }
---
+++ rt/normals_exotique.c
@@ -0,0 +1,38 @@
+/*
+** normals_exotique.c for rt in /home/debas_e//Desktop/projet/rt/rt
+**
+** Made by etienne debas
+** Login   <debas_e@epitech.net>
+**
+** Started on  Fri Jun  7 19:54:06 2013 etienne debas
+** Last update Fri Jun  7 19:55:30 2013 etienne debas
+*/
+
+#include <math.h>
+#include "main.h"
+#include "light.h"
+
+void		tangle_normal(double *nvec, double *obj_coor,
+			      double *pert, t_obj *obj)
+{
+  nvec[0] = 4 * pow(obj_coor[0], 3) - 20 * pow(obj->size[0], 2) * obj_coor[0];
+  nvec[1] = 4 * pow(obj_coor[1], 3) - 20 * pow(obj->size[0], 2) * obj_coor[1];
+  nvec[2] = 4 * pow(obj_coor[2], 3) - 20 * pow(obj->size[0], 2) * obj_coor[2];
+  perturb_normal(nvec, obj_coor, pert);
+}
+
+void		tore_normal(double *nvec, double *obj_coor,
+			    double *pert, t_obj *obj)
+{
+  double	k;
+
+  k = 4 * (pow(obj_coor[0], 2)
+           + pow(obj_coor[1], 2)
+           + pow(obj_coor[2], 2)
+           + pow(obj->size[0], 2)
+           - pow(obj->size[1], 2));
+  nvec[0] = obj_coor[0] * k - 8.0 * pow(obj->size[0], 2) * obj_coor[0];
+  nvec[1] = obj_coor[1] * k - 8.0 * pow(obj->size[0], 2) * obj_coor[1];
+  nvec[2] = obj_coor[2] * k;
+  perturb_normal(nvec, obj_coor, pert);
+}
