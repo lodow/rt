04f430c418a19f6a51f691e9db681d20ea2e97df
Adrien <dmadrien@msn.com>
2013-06-09 11:46:04 +0200

    Trick .obj

--- rt/src/pars_convert_model_tri.c
+++ rt/src/pars_convert_model_tri.c
@@ -5,24 +5,24 @@
 ** Login   <adrien@mint>
 **
 ** Started on  Wed May  8 18:10:25 2013 Adrien Della Maggiora
-** Last update Sat Jun  8 17:36:12 2013 luc sinet
+** Last update Sun Jun  9 11:31:57 2013 adrien dellamaggiora
 */
 
 #include "main.h"
 #include "model.h"
 
 void	convert_model_t_obj(double *vert, double *normal,
-                          UNUSED double *uvs, t_obj *obj)
+			    UNUSED double *uvs, t_obj *obj)
 {
-  obj->v1[0] = (vert[3] - vert[0]);
-  obj->v1[1] = (vert[4] - vert[1]);
-  obj->v1[2] = (vert[5] - vert[2]);
-  obj->v2[0] = (vert[6] - vert[0]);
-  obj->v2[1] = (vert[7] - vert[1]);
-  obj->v2[2] = (vert[8] - vert[2]);
-  obj->pos[0] += (vert[0]);
-  obj->pos[1] += (vert[1]);
-  obj->pos[2] += (vert[2] /* * obj->size[0] */);
+  obj->v1[0] = (vert[0] - vert[3]);
+  obj->v1[1] = (vert[1] - vert[4]);
+  obj->v1[2] = (vert[2] - vert[5]);
+  obj->v2[0] = (vert[6] - vert[3]);
+  obj->v2[1] = (vert[7] - vert[4]);
+  obj->v2[2] = (vert[8] - vert[5]);
+  obj->pos[0] += vert[3] / 2;
+  obj->pos[1] += vert[4] / 2;
+  obj->pos[2] += vert[5] / 2;
   obj->normal[0] = (normal[0] + normal[3] + normal[6]) / 3;
   obj->normal[1] = (normal[1] + normal[4] + normal[7]) / 3;
   obj->normal[2] = (normal[2] + normal[5] + normal[8]) / 3;
